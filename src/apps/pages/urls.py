396a5eeba9feac25ebd707c0d2eead5deb93b865
hojat-gazestani <hojatgazestani@gmail.com>
2025-05-14 18:11:21 +0330

    django https response

--- src/apps/pages/urls.py
+++ src/apps/pages/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import HomePageView, AboutPageView
+from .views import HomePageView, AboutPageView, home_page_view
 
 urlpatterns = [
         path("about/", AboutPageView.as_view(), name="about"),
+        path("home/", home_page_view, name="home"),
         path("", HomePageView.as_view(), name="home"),
 ]
--- src/apps/pages/views.py
+++ src/apps/pages/views.py
@@ -1,8 +1,12 @@
 
 from django.views.generic import TemplateView
+from django.http import HttpResponse
 
 class HomePageView(TemplateView):
     template_name = "home.html"
 
 class AboutPageView(TemplateView):
     template_name = "about.html"
+
+def home_page_view(request):
+    return HttpResponse("Hello, World!")
